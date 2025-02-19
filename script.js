732a9ae2f40c39e74808afef3d1c047f1af6b023
noadekocker <dekockernoa@gmail.com>
2025-02-19 16:46:39 +0100

    Nieuwe wijzigingen toegevoegd voor Thriftie

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-// Dit is een eenvoudige JavaScript om een knopactie te doen
-document.querySelector("button").addEventListener("click", function() {
-  alert("Get Started clicked!");
-});
+async function runClarifaiWorkflow() {
+    const fileInput = document.getElementById('imageUpload');
+    if (!fileInput.files.length) {
+        alert("Upload een afbeelding!");
+        return;
+    }
+
+    const reader = new FileReader();
+    reader.readAsDataURL(fileInput.files[0]);
+    reader.onload = async function () {
+        const base64Image = reader.result.split(',')[1];
+
+        const response = await fetch('https://api.clarifai.com/v2/workflows/4zg98pruwzj4/results', {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Key ecb507c8243b4a05bd983da95ca73790',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                inputs: [{ data: { image: { base64: base64Image } } }]
+            })
+        });
+
+        const data = await response.json();
+        document.getElementById('result').innerText = JSON.stringify(data, null, 2);
+    };
+}
